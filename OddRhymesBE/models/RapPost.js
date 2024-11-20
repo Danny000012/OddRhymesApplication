610c8d1ab8a5a190d0e06af337e344673e347af3
Danny Selowa <DSelowa@oldmutual.com>
2024-11-20 17:40:00 +0200

    implemented the rating system

--- OddRhymesBE/models/RapPost.js
+++ OddRhymesBE/models/RapPost.js
@@ -8,6 +8,13 @@ const RapPostSchema = new mongoose.Schema({
     text: { type: String, required: true },
     createdAt: { type: Date, default: Date.now }
   }],
+  ratings: [{
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    rating: { type: Number, required: true, min: 1, max: 5 }
+  }],
+  ratingCount: { type: Number, default: 0 }, // to keep track of the number of ratings
+  averageRating: { type: Number, default: 0 },
+  isRatingFinal: { type: Boolean, default: false }, // to know if the rating is set from the first 5
   createdAt: { type: Date, default: Date.now }
 });
 
